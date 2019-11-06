f9c605438050af890d36c887fe3e84fab4376525
Chuanren Wu <contact@thedrwu.com>
2019-11-06 15:57:42 +0100

    Use round joins

--- adjust-lm.sh
+++ adjust-lm.sh
@@ -3,11 +3,8 @@
 # ExpandStroke by 2*6 is OK, with 3*6 there seems to be no problem and looks
 # similar to the books using T3 CM (e.g. Toroidally Confined Plasma by R.B.White)
 
-for i in origin/lmroman*.otf ;  do
-    fontforge adjust-lm-stroke.pe "${i#origin/}" 3 6 &
+for i in origin/lmroman*.otf origin/latinmodern-math.otf ;  do
+    fontforge adjust-lm-stroke.pe "${i#origin/}" &
 done
-
-# \symscr{L} would have problem with 3*6, switch to 1*18 for math
-fontforge adjust-lm-stroke.pe latinmodern-math.otf 1 18 &
 
 wait
