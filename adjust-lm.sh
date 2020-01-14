1eb1d55dfb21c26e27bfadcf27a4fddf5d121a9e
Chuanren Wu <chuanren.wu@kit.edu>
2020-01-14 10:04:27 +0100

    Add type1 fonts

--- adjust-lm.sh
+++ adjust-lm.sh
@@ -3,8 +3,24 @@
 # ExpandStroke by 2*6 is OK, with 3*6 there seems to be no problem and looks
 # similar to the books using T3 CM (e.g. Toroidally Confined Plasma by R.B.White)
 
-for i in origin/lmroman*.otf origin/latinmodern-math.otf ;  do
-    fontforge adjust-lm-stroke.pe "${i#origin/}" &
-done
+trap "killall fontforge" SIGINT
+
+fakebold() {
+    for i in origin/* ;  do
+        fontforge adjust-lm-stroke.pe "${i#origin/}" &
+    done
+}
+
+rm origin > /dev/null
+
+ln -s otf origin
+fakebold
 
 wait
+rm origin
+
+ln -s pfb origin
+fakebold
+
+wait
+rm origin
